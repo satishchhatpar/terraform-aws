0e8111f78a62e7c6e7e4ff3768b155d1e42d9620
Satish Chhatpar <Satish.chhatpar@gmail.com>
2022-12-06 11:34:47 +0530

    Creating main.tf

---
+++ main.tf
@@ -0,0 +1,14 @@
+terraform {
+  required_providers {
+    aws = {
+      source = "hashicorp/aws"
+      version = "4.45.0"
+    }
+  }
+}
+
+provider "aws" {
+  # Configuration options
+  region = "us-east-1"
+  profile = "my-dev-profile"
+}
